3ace6d8d04c84427444c10694b5a1588cf278e89
Grégory <gregory_bevan@hotmail.com>
2017-03-14 00:42:12 +0100

    Add createRandomPosition function

--- backend/src/main/kotlin/fr/elgregos/guitar/GuitarStrings.kt
+++ backend/src/main/kotlin/fr/elgregos/guitar/GuitarStrings.kt
@@ -2,4 +2,8 @@ package fr.elgregos.guitar
 
 val strings = mapOf(1 to 'E', 2 to 'A', 3 to 'D', 4 to 'G', 5 to 'B', 6 to 'E')
 
-data class Position(val guitarString: Int, val fret: Int)
+fun createRandomPosition(): Position {
+	return Position(1, 0, "Mi")
+}
+
+data class Position(val guitarString: Int, val fret: Int, val note: String)
