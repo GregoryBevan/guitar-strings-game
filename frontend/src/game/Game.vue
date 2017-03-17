4f4d1bfdca04587b4cee509aa2246c143a984006
Grégory <gregory_bevan@hotmail.com>
2017-03-17 01:36:00 +0100

    It works

--- frontend/src/game/Game.vue
+++ frontend/src/game/Game.vue
@@ -17,24 +17,32 @@
     <div class="row col-xs-12 space"></div>
     <div class="row col-xs-12 form-group">
       <div class="col-xs-1" v-for="n in notes">
-        <button type="button" class="btn btn-info big-btn">{{n}}</button>
+        <button type="button" class="btn big-btn" v-bind:class="{'btn-info': selectedNote == '' || !(n == note),'btn-success': n == note, 'btn-danger': selectedNote == n && n != note}" v-on:click="checkResponse(n, $event)">{{n}}</button>
       </div>
+    </div>
+    <div class="row col-xs-12 text-center" v-bind:class="{'mask': selectedNote == ''}">
+        <br><br>
+        <button type="button" class="btn btn-primary" v-on:click="reload">Recommencer</button>
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 
 export default {
   name: 'game',
   data: ()=> ({
-    guitareString: 'E',
+    guitareString: '',
     fret: 0,
-    note: 'E',
-    notes: ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    note: '',
+    sortedNotes: ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"],
+    notes: [],
+    selectedNote: ''
   }),
   mounted() {
-    this.getNewPosition()
+    this.getNewPosition();
+    this.notes = _.shuffle(this.sortedNotes);
   },
   methods: {
     getNewPosition: function() {
@@ -45,8 +53,14 @@ export default {
         this.fret = data.fret;
         this.note = data.note;
       })
+    },
+    checkResponse: function(note, event) {
+      this.selectedNote = note;
+    },
+    reload: function() {
+      this.selectedNote = '';
+      this.getNewPosition();
     }
   }
 }
 </script>
-<
